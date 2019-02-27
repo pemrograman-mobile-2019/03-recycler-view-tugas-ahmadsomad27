755a677c3d1a831277d321d68a801de78e4e7802
ahmadsomad27 <asomad521@gmail.com>
2019-02-27 23:35:49 +0700

    penambahan code pada MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
    int[] IMAGES = {R.drawable.DSC_3507, R.drawable.dsc_0023, R.drawable.Adin, R.drawable.Erly,
@@ -21,25 +23,40 @@ public class MainActivity extends AppCompatActivity {
        setContentView(R.layout.activity_main);
 
        ListView ListView=(ListView) findViewById(R.id.ListView);
+
+       CustomAdapter customAdapter=new CustomAdapter();
+
+       ListView.setAdapter(customAdapter);
    }
 
-   class CustomAdapter extends BaseAdapter(
-        @Override
-        public int getCount(){
-            return IMAGES.length;
-        }
+   class CustomAdapter extends BaseAdapter {
+       @Override
+       public int getCount() {
+           return IMAGES.length;
+       }
 
-        @Override
-        public Object getItem(int i){
-            return null;
-        }
-        @Override
-        public long getItemId(int i){
-            return 0;
-        }
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup){
-            return null;
-        }
-            )
+       @Override
+       public Object getItem(int i) {
+           return null;
+       }
+
+       @Override
+       public long getItemId(int i) {
+           return 0;
+       }
+
+       @Override
+       public View getView(int i, View view, ViewGroup viewGroup){
+           view = getLayoutInflater().inflate(R.layout.customlayout, null);
+           ImageView imageView = (ImageView)view.findViewById(R.id.image);
+           TextView textView_name = (TextView)view.findViewById(R.id.textView_name);
+           TextView textView_descriptions = (TextView)view.findViewById(R.id.textView_description);
+
+           imageView.setImageResource(IMAGES[i]);
+           textView_name.setText(NAMES[i]);
+           textView_descriptions.setText(DESCRIPTIONS[i]);
+
+           return view;
+       }
+   }
 }
